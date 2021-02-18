4bfd93504b34fe8967bb8016f27b57a04cffa906
crb20 <crb20@aber.ac.uk>
2021-02-18 20:07:45 +0000

    up to date

--- AUSWPandroidapp/app/src/main/java/uk/ac/aber/dcs/cs39440/auswpandroidapp/ui/MainActivity.kt
+++ AUSWPandroidapp/app/src/main/java/uk/ac/aber/dcs/cs39440/auswpandroidapp/ui/MainActivity.kt
@@ -1,12 +1,12 @@
-package uk.ac.aber.dcs.cs39440.auswpandroidapp
+package uk.ac.aber.dcs.cs39440.auswpandroidapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import uk.ac.aber.dcs.cs39440.auswpandroidapp.R
 import uk.ac.aber.dcs.cs39440.auswpandroidapp.databinding.ActivityMainBinding
 
 
@@ -25,14 +25,14 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val navView = binding.bottomNavView
-        val navController = findNavController(R.id.nav_host_fragment)
+      val navController = findNavController(R.id.nav_host_fragment)
 
         val appBarConfiguration = AppBarConfiguration(setOf(
-        R.id.navigation_home, R.id.navigation_Committee, R.id.navigation_Events, R.id.navigation_Tracker
-        ))
-        setupActionBarWithNavController(navController,appBarConfiguration)
+            R.id.navigation_home, R.id.navigation_Committee, R.id.navigation_Events, R.id.navigation_Tracker))
+      setupActionBarWithNavController(navController,appBarConfiguration)
         navView.setupWithNavController(navController)
 
     }
+    
     }
 
