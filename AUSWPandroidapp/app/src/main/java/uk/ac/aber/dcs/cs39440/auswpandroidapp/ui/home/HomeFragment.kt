ecda1bb848cfa5b8490172211a90eaca5fe3c568
Callum Binner <callumbinner22@outlook.com>
2021-03-04 13:01:33 +0000

    Added database reference

--- AUSWPandroidapp/app/src/main/java/uk/ac/aber/dcs/cs39440/auswpandroidapp/ui/home/HomeFragment.kt
+++ AUSWPandroidapp/app/src/main/java/uk/ac/aber/dcs/cs39440/auswpandroidapp/ui/home/HomeFragment.kt
@@ -1,14 +1,27 @@
 package uk.ac.aber.dcs.cs39440.auswpandroidapp.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 import uk.ac.aber.dcs.cs39440.auswpandroidapp.R
+import uk.ac.aber.dcs.cs39440.auswpandroidapp.databinding.FragmentHomeBinding
+import kotlin.String as String
 
+private const val TAG = "My Activity"
 
 class HomeFragment : Fragment() {
+
+    private lateinit var homeFragmentBinding: FragmentHomeBinding
+    private lateinit var database: DatabaseReference
+
 
 
     override fun onCreateView(
@@ -16,8 +29,41 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        homeFragmentBinding = FragmentHomeBinding.inflate(inflater,container,false)
+
+        val texter = homeFragmentBinding.testTextView
+        
+       database = Firebase.database.reference.child("Events")
+
+
+
+        val postListener = object: ValueEventListener{
+            override fun onDataChange(datasnapshot: DataSnapshot) {
+                val post = datasnapshot.getValue()
+                texter.text = post.toString()
+                Log.d(TAG, "Value is: $post")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        }
+        database.addValueEventListener(postListener)
+
+
+
+        return homeFragmentBinding.root
+
+
+
+
+
+
     }
+
+
+
 
 
 }
