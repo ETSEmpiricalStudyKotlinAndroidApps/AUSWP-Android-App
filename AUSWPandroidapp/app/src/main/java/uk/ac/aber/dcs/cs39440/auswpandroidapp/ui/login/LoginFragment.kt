b870dbf4b97414ed0b4077c5b12906c954bc5840
Callum Binner <callumbinner22@outlook.com>
2021-04-13 18:14:22 +0100

    Added firebase auth to allow for account registration and signin

--- AUSWPandroidapp/app/src/main/java/uk/ac/aber/dcs/cs39440/auswpandroidapp/ui/login/LoginFragment.kt
+++ AUSWPandroidapp/app/src/main/java/uk/ac/aber/dcs/cs39440/auswpandroidapp/ui/login/LoginFragment.kt
@@ -1,5 +1,6 @@
 package uk.ac.aber.dcs.cs39440.auswpandroidapp.ui.login
 
+import android.content.ContentValues.TAG
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.annotation.StringRes
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,6 +18,8 @@ import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 import uk.ac.aber.dcs.cs39440.auswpandroidapp.R
 import uk.ac.aber.dcs.cs39440.auswpandroidapp.databinding.FragmentLoginBinding
@@ -23,6 +27,10 @@ import uk.ac.aber.dcs.cs39440.auswpandroidapp.databinding.FragmentLoginBinding
 class LoginFragment : Fragment() {
 
 private lateinit var LoginFragmentBinding: FragmentLoginBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var login : Button
+    private lateinit var email : EditText
+    private lateinit var password : EditText
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -32,18 +40,51 @@ private lateinit var LoginFragmentBinding: FragmentLoginBinding
 
         LoginFragmentBinding = FragmentLoginBinding.inflate(inflater, container, false)
 
-
+        auth = FirebaseAuth.getInstance()
+        val firebaseUser: FirebaseUser? = auth.currentUser
         val register = LoginFragmentBinding.SignUp
 
+        email = LoginFragmentBinding.username
+        password = LoginFragmentBinding.password
+        login = LoginFragmentBinding.Login
+
         register.isClickable
+        login.isClickable
+
 
         register.setOnClickListener {
          val navController = findNavController()
             navController.navigate(R.id.action_loginFragment_to_registerFragment)
         }
 
+
+        signin()
+
         return LoginFragmentBinding.root
     }
 
+        private fun signin(){
+            login.setOnClickListener {
+                signinUser(email.text.toString().trim(),password.text.toString().trim())
+                                    }
+                }
 
-}
+    private fun signinUser(email: String, password: String){
+
+
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { signIn ->
+                    if (signIn.isSuccessful){
+                        Toast.makeText(context,"Welcome $email", Toast.LENGTH_SHORT).show()
+                        Log.d(TAG, "Sign in successful")
+                        val navController = findNavController()
+                        navController.navigate(R.id.navigation_home)
+
+                    }else{
+                        Toast.makeText(context,"Something went wrong! Please try again", Toast.LENGTH_SHORT)
+                    }
+                }
+        }
+    }
+
--- AUSWPandroidapp/app/src/main/java/uk/ac/aber/dcs/cs39440/auswpandroidapp/ui/login/RegisterFragment.kt
+++ AUSWPandroidapp/app/src/main/java/uk/ac/aber/dcs/cs39440/auswpandroidapp/ui/login/RegisterFragment.kt
@@ -1,58 +1,77 @@
 package uk.ac.aber.dcs.cs39440.auswpandroidapp.ui.login
 
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.ktx.Firebase
 import uk.ac.aber.dcs.cs39440.auswpandroidapp.R
+import uk.ac.aber.dcs.cs39440.auswpandroidapp.databinding.FragmentRegisterBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var auth:FirebaseAuth
+    private lateinit var registerFragmentBinding: FragmentRegisterBinding
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var name: EditText
+    private lateinit var register: Button
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+
+        registerFragmentBinding = FragmentRegisterBinding.inflate(inflater, container, false)
+        auth = FirebaseAuth.getInstance()
+        val firebaseUser: FirebaseUser? = auth.currentUser
+
+        email = registerFragmentBinding.editTextTextEmailAddress
+        password = registerFragmentBinding.editTextTextPassword
+        name = registerFragmentBinding.editTextTextPersonName
+        register = registerFragmentBinding.button2
+
+        Register()
+
+        return registerFragmentBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                RegisterFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+
+    private fun Register(){
+        register.setOnClickListener {
+            createAccount(email.text.toString().trim(),password.text.toString().trim())
+        }
     }
+
+    private fun createAccount(email:String, password: String){
+       auth.createUserWithEmailAndPassword(email, password)
+           .addOnCompleteListener { task ->
+               if (task.isSuccessful){
+                   Log.d(TAG,"Account Created Successfully")
+                   Toast.makeText(context,"Create account successfully",Toast.LENGTH_SHORT).show()
+                   val navController = findNavController()
+                   navController.navigate(R.id.loginFragment)
+               }else{
+                   Log.w(TAG,"Authorisation Failed")
+                   Toast.makeText(context, "Authorisation failed", Toast.LENGTH_SHORT).show()
+               }
+           }
+    }
+
+
 }
+
+
